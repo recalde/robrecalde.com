a2b64aa3b30d8eb894ad51026365961f93f30666
Rob Recalde <recalde@gmail.com>
2022-06-22 16:50:10 -0400

    Deleted build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
-./dotnet6/dotnet --version
-./dotnet6/dotnet publish recalde-blazor/recalde-blazor.csproj -c Release -o output
