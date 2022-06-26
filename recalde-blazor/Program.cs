e0828c9d52a7f559a66f433f724e8cceca133d48
Robert Recalde <recalde@gmail.com>
2022-06-26 17:17:22 -0400

    Added http post to api.robrecalde.com/books

--- recalde-blazor/Program.cs
+++ recalde-blazor/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Builder;
 using recalde_blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+await app.RunAsync();
